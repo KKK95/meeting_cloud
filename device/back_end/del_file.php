c2b529c95cc22944ec15b6adeefb9e16d6d7d7f1
KKK95 <mkyang95@gmail.com>
2016-09-10 21:18:29 +0800

    Add files via upload

--- device/back_end/del_file.php
+++ device/back_end/del_file.php
@@ -1,25 +1,24 @@
 <?php
 
+
 		if(!isset($_SESSION))
 		{  	session_start();	}			//用 session 函式, 看用戶是否已經登錄了
 
 		require_once("../../connMysql.php");			//引用connMysql.php 來連接資料庫
-		
+	
 		require_once("login_check.php");
 		
 		$path = '../';
 		
-		if ( isset($_POST['name']) && isset($_GET['basic_path']) )			//無論如何一定會有 basic_path
+		if ( isset($_GET['basic_path']) && isset($_GET['file_name']) )
 		{
-			$path = $path . $_GET['basic_path']."/".$_POST['name'];
-			mkdir($path);
-		}	
-		else
-		{
-			echo "nothing value";
+			$path = $path.$_GET['basic_path']."/".$_GET['file_name'];
+			
+			if(file_exists($path))
+			{	unlink($path);	}	//將檔案刪除
+			else
+			{	echo"檔案不存在"	}
 		}
-		
-		echo "$path";
 		
 		if ( false !== ($rst = strpos($_GET['basic_path'], "user_upload_space")) )		//在我的空間
 			header("Location: my_upload_space.php?basic_path=".$_GET['basic_path']); 
