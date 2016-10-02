6c564354d70245f4c7954b6f1da3f2837ba77636
KKK95 <mkyang95@gmail.com>
2016-10-02 18:02:16 +0800

    Add files via upload

---
+++ device/back_end/create_meeting_scheduler.php
@@ -0,0 +1,29 @@
+<?php
+
+	header("Content-Type: text/html; charset=UTF-8");
+	
+	if(!isset($_SESSION))
+	{  		session_start();	}			//用 session 函式, 看用戶是否已經登錄了
+
+	require_once("../../connMysql.php");			//引用connMysql.php 來連接資料庫
+	
+	require_once("login_check.php");			
+	
+	$group_id = $_POST["group_id"];
+	$today = date("Y-m-d H:i:s");
+	$meeting_title = $_POST['meeting_title'];
+	$conference_sponsors_id = $_POST['conference_sponsors_id'];
+	$meeting_time = $_POST['meeting_time'];
+	
+	if ($group_id != "" && $conference_sponsors_id != "")
+	{
+		if (((strtotime($meeting_time) - strtotime($today))/ (60*60)) > 0)				//新增的會議必定不能在過去
+		{
+			$sql = "INSERT INTO meeting_scheduler value('', '".$group_id."', '".$meeting_title."', '".$conference_sponsors_id."', '".$meeting_time."')";
+			$result = $conn->query($sql);
+		}
+	}
+
+		header("Location: employee_center.php");
+	
+?>
